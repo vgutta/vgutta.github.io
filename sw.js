bfb4cb2a753f1186a536d925eee1d3f42ba35cd1
vgutta <vineethg@udel.edu>
2024-04-15 17:44:50 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3e15d59eb6a3b7307eaa.js"
+    "url": "webpack-runtime-1b423d89573443790628.js"
   },
   {
     "url": "framework-6c3ead24af170cdcb87c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7585b57a1c481ec6fab38f3970d0a8e0"
+    "revision": "fe8dc146ccda40bec7b354fa3a6e46fb"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-260e89c911c4cfae04b1.js"
